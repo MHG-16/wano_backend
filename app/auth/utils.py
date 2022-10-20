7633d9ef198685f450682ce386eaed8904908d87
mhg-16 <hammahadi2006@gmail.com>
2022-10-20 10:30:12 +0200

    insert facture

--- app/auth/utils.py
+++ app/auth/utils.py
@@ -2,32 +2,18 @@
 import secrets
 from flask import jsonify
 
+from app.user.models import Users
 from app.utils.auth_utils import check_login_by_email
 from app.utils.db_utils import updatequery
+from app.utils.database import SESSION
 
 
-def verify_email_and_password(email: str, password: str):
+def verify_email_and_password(email: str, password: str) -> dict:
     login = check_login_by_email(email, password)
     if login == -1:
         return jsonify({"error": True, "message": "Unkown email"}), 400
     if login == 1:
-        res = update_token_by_email(email)
-        if res:
-            return (
-                jsonify(
-                    {
-                        "error": False,
-                        "message": {
-                            "access_token": res,
-                        },
-                    }
-                ),
-                200,
-            )
-        return (
-            jsonify({"error": True, "message": "Server error(Access Token update)"}),
-            405,
-        )
+        return update_token_get_id_user(email)
     return jsonify({"error": True, "message": "Password is incorrect"}), 400
 
 
@@ -35,6 +21,29 @@ def update_token_by_email(email: str):
     token = secrets.token_hex()
     query = f"UPDATE users SET access_token = '{token}' where email = '{email}'"
     return token if updatequery(query) else None
+
+
+def update_token_get_id_user(email: str) -> dict:
+    if res := update_token_by_email(email):
+        return (
+            jsonify(
+                {
+                    "error": False,
+                    "message": {
+                        "access_token": res,
+                        "id_user": SESSION.query(Users)
+                        .filter(Users.email == email)
+                        .first()
+                        .id_user,
+                    },
+                }
+            ),
+            200,
+        )
+    return (
+        jsonify({"error": True, "message": "Server error(Access Token update)"}),
+        405,
+    )
 
 
 def update_token_by_access_token(access_token: str):
